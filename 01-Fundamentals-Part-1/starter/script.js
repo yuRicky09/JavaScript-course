b3c0d02cc656fc37ef3ea0428ce246eb25c009ad
yuricky <leoleo5994@gmail.com>
2021-04-03 12:40:02 +0800

    開始學習js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,60 @@
+/*let js = 'amazing';
+console.log("Jonas");
+console.log(23);
+
+let country = 'Taiwan';
+let continent = "Asian";
+let population = "23";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+/*
+const now = 2037;
+const ageLeo = now - 1994;
+const ageYu = now - 1996;
+console.log(ageLeo, ageYu);
+
+console.log(ageLeo * 2, ageLeo / 2, 2 ** 3);
+
+console.log('yu' + ' ' + 'leo');
+
+
+
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 4;
+x++; // x = x + 1
+x--;
+console.log(x);
+*/
+
+
+/*
+let marksWeights = 78, marksTall = 1.67;
+let johnWeights = 92, johnTall = 1.95;
+
+let bmi = marksWeights / marksTall ** 2;
+console.log(bmi);
+bmi = johnWeights / johnTall ** 2;
+console.log(bmi);
+
+marksWeights = 95, marksWeights = 1.88;
+johnWeights = 85, johnTall = 1.76;
+console.log(bmi = marksWeights / marksTall ** 2);
+console.log(bmi = johnWeights / johnTall ** 2);
+*/
+
+
+const firstName = "HSU";
+const lastName = "YU";
+const age = 26;
+const job = "engineer";
+
+console.log(`${firstName} ${lastName} is ${age} years old!He job is ${job}.`)
+
+console.log(`123456
+heyheyhey
+comeon man!!
+......`)
