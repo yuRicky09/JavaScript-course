4b1a8bc57f0a5512fe3c2137a0dc12fd8a9c6a1c
yuricky <leoleo5994@gmail.com>
2021-04-16 01:32:40 +0800

    練習題

---
+++ 08-Behind-the-Scenes/scripthw.js
@@ -0,0 +1,27 @@
+// const array = [1, 1];
+// function fibonacci() {
+//   array = array.push(array[0] + array[1]);
+//   console.log(array);
+// }
+
+// fibonacci(); // 可在畫面上印出小於 100 的數列
+// // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+// const array1 = [1, 4, 9, 16];
+
+// // pass a function to map
+// const map1 = array1.map(x => x[] * 2);
+
+// console.log(map1);
+// // expected output: Array [2, 8, 18, 32]
+
+const array = [0, 1];
+
+function fib(n) {
+  for (i = 2; i < n; i++) {
+    const a = array[i - 1];
+    const b = array[i - 2];
+    array.push(a + b);
+    console.log(array);
+  }
+}
+fib(8);
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -11,10 +11,14 @@ function calcAge(birthYear) {
   printAge(); // 可以順利印出 一樣遵循look up varible原則
 
   if (birthYear >= 1981 && birthYear <= 1996) {
+    // 在子層內定義完全一樣的變數名  (完全沒問題 兩者可看做不同東西)
     const firstName = 'Steven';
+    //  如果是賦予值的話就會更新原先的內容
+    output = 'NEW! OUTPUT!!';
+
     var millenial = true;
     const str = `oh, and you're a millenial, ${firstName}`;
-    console.log(str);
+    console.log(str); //這邊會印出Steven 因為他在自己的scope內就找到了firstName這個變數，所以她不會往上找。
 
     function add(a, b) {
       return a + b;
@@ -26,5 +30,14 @@ function calcAge(birthYear) {
   return age;
 }
 
-const firstName = 'leo';
+const firstName = 'leo'; //有兩個同名的變數是完全沒問題的，只要他們定義在不同的scope，他們就是完全不相等的東西。我們寫function的參數(paramater)時也是相同道理，因為他們各自存在自己的scope所以可以重複
 calcAge(1991);
+
+const array = [1, 1];
+function fibonacci(n) {
+  array = array.push(array[0] + array[1]);
+  console.log(array);
+}
+
+fibonacci(100); // 可在畫面上印出小於 100 的數列
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
