6e8ced821a0cd33e7fcba2989e9cc5794018fa18
yuricky <leoleo5994@gmail.com>
2021-04-22 03:29:39 +0800

    learn function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const bookings = [];
+//  預設參數的直可以是任何運算式
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    // ES6寫法 當key: value 是一樣時的簡寫 ex name: name =>name
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
