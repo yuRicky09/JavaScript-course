84bf020a4b6852d3f381b4ad14a7ef6e11bfdc40
yuricky <leoleo5994@gmail.com>
2021-04-22 02:04:44 +0800

    finish coding challenge4

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -593,7 +593,7 @@ for (const [min, event] of gameEvents) {
 */
 
 // ------------ String
-
+/*
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
 // 字串也可以用索引值
@@ -711,3 +711,59 @@ const planesInline = function (n) {
 planesInline(2);
 planesInline(5);
 planesInline(6);
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const btn = document.querySelector('button');
+let arr = [];
+btn.addEventListener('click', function () {
+  let text = document.querySelector('textarea').value;
+  console.log(text);
+  const newText = text.split('\n');
+  console.log(newText);
+  for (let [i, textItem] of newText.entries()) {
+    // console.log(textItem);
+    let [first, second] = textItem.toLowerCase().trim().split('_');
+    textItem = first + second[0].toUpperCase() + second.slice(1);
+    // textItem = textItem.replace('_', '');
+    console.log(textItem);
+    textItem = textItem.padEnd(20, '·') + '🚗'.repeat(i + 1);
+    console.log(textItem);
+    arr.push(textItem);
+  }
+  console.log(arr);
+  // text = arr.join('\n');
+});
