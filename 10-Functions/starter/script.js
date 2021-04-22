36b53c7b2b2695dc9a548f515b9f315cd15d8e73
yuricky <leoleo5994@gmail.com>
2021-04-23 00:09:59 +0800

    finish function lesson

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -280,6 +280,7 @@ console.dir(f); // closure變成h();
 const boardPassengers = function (n, wait) {
   const perGroup = n / 3;
 
+  // 這邊也產生了closure 父層fn的參數也是它的變數 所以子層fn也能讀取
   setTimeout(function () {
     console.log(`We are now borading all ${n} passengers`);
     console.log(`There are 3 groups, each with ${perGroup} passengers`);
@@ -287,3 +288,24 @@ const boardPassengers = function (n, wait) {
 };
 
 boardPassengers(180, 3);
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
